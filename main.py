635c473b56d250c7e7902c393839e66f6e86cc60
Jamesotim <Jamesotimaojing@gmail.com>
2026-09-11 15:50:39 +0300

    fix main.py implementation

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+from utils import add_numbers, multiply_numbers
+
+
+def main():
+    print("Python Lab")
+
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+
+    print(f"Sum: {add_numbers(a, b)}")
+    print(f"Product: {multiply_numbers(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
